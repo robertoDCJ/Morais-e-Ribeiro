0557c502ebc95c9a14c412f8720fc465eff77ab5
robertoDCJ <robertox70@hotmail.com>
2024-05-13 18:23:57 -0300

    Changed names of the images

--- src/components/ContolledCarousel/ControlledCarousel.tsx
+++ src/components/ContolledCarousel/ControlledCarousel.tsx
@@ -12,32 +12,32 @@ function ImgCarousel({ ...props }: ImgCarouselProps) {
     <div className="bg-cover bg-center flex h-659 w-full" {...props}></div>
   );
 }
-const TextCarousel = () =>{
-    return(
-            <div className="grid grid-cols-1 place-items-center max-w-screen-xl w-full md:place-items-start md:ml-52 p-12 ">
-              <div className="md:w-456">
-                <h1 className=" md:text-42 text-3xl font-bold mb-2">
-                  Morais & Ribeiro
-                </h1>
-                <h2 className="md:text-2xl text-base font-bold antialiased">
-                  Somos mais do que apenas advogados - somos uma parceria fundada na
-                  amizade e no profissionalismo.
-                </h2>
-                <div className="border border-white my-3 antialiased w-full"></div>
-                <p className="font-Lato font-normal md:text-lg text-sm mb-3">
-                  Confie em nós para representar seus interesses com paixão,
-                  profissionalismo e comprometimento. Somos um escritório de atuação
-                  full service, 360º, adaptando,entendendo e acolhendo cada cliente com
-                  sua respectiva demanda.
-                </p>
-                <p className="font-Lato font-normal md:text-lg text-sm">
-                  Estamos honrados em fazer parte de sua jornada jurídica e ansiosos
-                  para trabalhar juntos na busca por soluções eficazes e justas.
-                </p>
-              </div>
-            </div>
-    )
-}
+const TextCarousel = () => {
+  return (
+    <div className="grid grid-cols-1 place-items-center max-w-screen-xl w-full md:place-items-start md:ml-52 p-12 ">
+      <div className="md:w-456">
+        <h1 className=" md:text-42 text-3xl font-bold mb-2">
+          Morais & Ribeiro
+        </h1>
+        <h2 className="md:text-2xl text-base font-bold antialiased">
+          Somos mais do que apenas advogados - somos uma parceria fundada na
+          amizade e no profissionalismo.
+        </h2>
+        <div className="border border-white my-3 antialiased w-full"></div>
+        <p className="font-Lato font-normal md:text-lg text-sm mb-3">
+          Confie em nós para representar seus interesses com paixão,
+          profissionalismo e comprometimento. Somos um escritório de atuação
+          full service, 360º, adaptando,entendendo e acolhendo cada cliente com
+          sua respectiva demanda.
+        </p>
+        <p className="font-Lato font-normal md:text-lg text-sm">
+          Estamos honrados em fazer parte de sua jornada jurídica e ansiosos
+          para trabalhar juntos na busca por soluções eficazes e justas.
+        </p>
+      </div>
+    </div>
+  );
+};
 export const ControlledCarousel = ({ children }: CarouselProps) => {
   const [index, setIndex] = useState(0);
 
@@ -49,81 +49,88 @@ export const ControlledCarousel = ({ children }: CarouselProps) => {
 
   return (
     <div className="font-Alegreya">
-      <Carousel interval={10000} activeIndex={index} onSelect={handleSelect} fade>
+      <Carousel
+        interval={10000}
+        activeIndex={index}
+        onSelect={handleSelect}
+        fade
+      >
         <Carousel.Item>
-          <ImgCarousel className={`grid place-items-center ${
-          isAnimationActive ? "animate-slide" : "bg-center"
-        }`}
+          <ImgCarousel
+            className={`grid place-items-center ${
+              isAnimationActive ? "animate-slide" : "bg-center"
+            }`}
             style={{
               backgroundPosition: "center",
-              backgroundImage: "url('/ImgCarousel/img1.png')",
+              backgroundImage: "url('/ImgCarousel/Img1.png')",
               backgroundSize: "cover",
               height: "659px",
             }}
-          > 
-           <TextCarousel/>
+          >
+            <TextCarousel />
           </ImgCarousel>
         </Carousel.Item>
 
         <Carousel.Item>
-        <ImgCarousel className={`grid place-items-center ${
-          isAnimationActive ? "animate-slide" : "bg-center"
-        }`}
-            style={{ 
-              backgroundImage: "url('/ImgCarousel/img2.png')",
+          <ImgCarousel
+            className={`grid place-items-center ${
+              isAnimationActive ? "animate-slide" : "bg-center"
+            }`}
+            style={{
+              backgroundImage: "url('/ImgCarousel/Img2.png')",
               backgroundSize: "cover",
               height: "659px",
             }}
           >
-             <TextCarousel/>
-           </ImgCarousel>
+            <TextCarousel />
+          </ImgCarousel>
         </Carousel.Item>
 
         <Carousel.Item>
-        <ImgCarousel className={`grid place-items-center ${
-          isAnimationActive ? "animate-slide" : "bg-center"
-        }`}
-            style={{ 
-              backgroundImage: "url('/ImgCarousel/img3.png')",
+          <ImgCarousel
+            className={`grid place-items-center ${
+              isAnimationActive ? "animate-slide" : "bg-center"
+            }`}
+            style={{
+              backgroundImage: "url('/ImgCarousel/Img3.png')",
               backgroundSize: "cover",
               height: "659px",
             }}
           >
-             <TextCarousel/>
-           </ImgCarousel>
+            <TextCarousel />
+          </ImgCarousel>
         </Carousel.Item>
 
         <Carousel.Item>
-        <ImgCarousel className={`grid place-items-center ${
-          isAnimationActive ? "animate-slide" : "bg-center"
-        }`}
-            style={{ 
-              backgroundImage: "url('/ImgCarousel/img4.png')",
+          <ImgCarousel
+            className={`grid place-items-center ${
+              isAnimationActive ? "animate-slide" : "bg-center"
+            }`}
+            style={{
+              backgroundImage: "url('/ImgCarousel/Img4.png')",
               backgroundSize: "cover",
               height: "659px",
             }}
           >
-             <TextCarousel/>
-           </ImgCarousel>
+            <TextCarousel />
+          </ImgCarousel>
         </Carousel.Item>
 
         <Carousel.Item>
-        <ImgCarousel className={`grid place-items-center ${
-          isAnimationActive ? "animate-slide" : "bg-center"
-        }`}
-            style={{ 
-              backgroundImage: "url('/ImgCarousel/img5.png')",
+          <ImgCarousel
+            className={`grid place-items-center ${
+              isAnimationActive ? "animate-slide" : "bg-center"
+            }`}
+            style={{
+              backgroundImage: "url('/ImgCarousel/Img5.png')",
               backgroundSize: "cover",
               height: "659px",
             }}
           >
-             <TextCarousel/>
-           </ImgCarousel>
+            <TextCarousel />
+          </ImgCarousel>
         </Carousel.Item>
-
-        </Carousel>
+      </Carousel>
     </div>
   );
 };
-
-
